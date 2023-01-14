c308e0b8d64613795dc65dce3cb108afa51ba6de
James Kelly <jk377y@gmail.com>
2023-01-14 10:52:17 -0600

    edited api and html routers, added more detailed comments, added post route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,18 @@
 const express = require('express');  // imports express
-const apiRouter = express.Router();  // 
-const fs = require('fs');  // gives access to file system module
-const path = require('path');
+const apiRouter = express.Router();  // creates an instance of the Express object
+const fs = require('fs');  // gives access to file system module which allows us to read/write files
+const path = require('path');  // gives access to the path module which gives access to file pathing
 const uuid = require('uuid');  // imports uuid npm package
 
-// this is the GET route to load the index file when only the api url is entered; for example http://localhost:3001
-htmlRouter.get('*', (req, res) => {   // when GET localhost:3001, index.html is displayed
-res.sendFile(path.join(__dirname, '..', '/develop/public/index.html'));
-});
-
-// this is the GET route to read all notes from db.json file when api/notes is in the url; for example http://localhost:3001/notes
-apiRouter.get('/notes', (req, res) => {
-    let data = fs.readFileSync(path.join(__dirname, '..', '/develop/db/db.json'));
-    // response is the data parsed as json
-    res.json(JSON.parse(data));
+// this will happen when the GET api/notes route is executed; read all notes from db.json file when api/notes is in the url; for example http://localhost:3001/notes; then parse the data
+apiRouter.get('/notes', (req, res) => {  // when the GET api/notes route is executed
+    let data = fs.readFileSync(path.join(__dirname, '..', '/develop/db/db.json'));  // the db.json file will be read also
+    res.json(JSON.parse(data));  //  response is the data parsed as json
     });
-  
+
+// this is the POST api/notes route
+    apiRouter.post('/notes', (req, res) => {  // 
+
+    });
+
 module.exports = apiRouter;

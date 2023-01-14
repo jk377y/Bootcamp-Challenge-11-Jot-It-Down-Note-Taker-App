e8b49cfff2436df8890c8b2112bc4e05db408fb9
James Kelly <jk377y@gmail.com>
2023-01-14 12:33:26 -0600

    changed PORT @ app.listen on server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');  // import express module
 const app = express();  // create an express application instance
 const path = require('path');  // import path module
-const PORT = process.env.PORT || 3001;  // define the PORT at 3001
+const port = process.env.PORT || 3001;  // define the PORT at 3001
 const apiRouter = require('./routes/apiRoutes');  // import apiRoutes module
 const htmlRouter = require('./routes/htmlRoutes');  // import htmlRoutes module
 
@@ -11,6 +11,6 @@ app.use('/api', apiRouter);  // use the apiRouter for all routes starting with '
 app.use('/', htmlRouter);  // use the htmlRouter for all routes starting with '/'
 
 
-app.listen(PORT, () => {  // start the server and listen on PORT 3001
-  console.log(`Server started on port ${PORT}`);  // log a message to the console when the server starts
+app.listen(port, () => {  // start the server and listen on PORT 3001
+  console.log(`Server started on port ${port}`);  // log a message to the console when the server starts
 });
